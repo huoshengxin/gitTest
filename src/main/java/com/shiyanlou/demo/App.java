ba5c6158c7f138b90cac876b8825c9e2addcb39f
huoshengxin <huoshengxin@163.com>
2019-09-22 01:16:54 +0800

    实验楼2019-09-22 01:16:52

--- src/main/java/com/shiyanlou/demo/App.java
+++ src/main/java/com/shiyanlou/demo/App.java
@@ -3,11 +3,21 @@ package com.shiyanlou.demo;
 import com.shiyanlou.demo.helloworld.HelloWorld;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.shiyanlou.demo.loosely_coupled.OutputHelper;
 
 public class App {
     private static ApplicationContext context;
 
-    public static void main(String[] args) {
+    public static void main( String[] args )
+    {
+        context = new ClassPathXmlApplicationContext(new String[] {"Spring-Output.xml"});
+
+        OutputHelper output = (OutputHelper)context.getBean("OutputHelper");
+        output.generateOutput();
+    }
+
+
+    public static void main测试1(String[] args) {
         context = new ClassPathXmlApplicationContext("SpringBeans.xml");
 
         HelloWorld obj = (HelloWorld) context.getBean("helloBean");
